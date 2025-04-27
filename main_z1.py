86ad81bb9e0dbebf8f701b7ff63389a11584b613
OrionFLASH <orionflash-msk@yandex.ru>
2025-04-27 12:39:43 +0300

    Add Task 3 Polymorphism and magic methods

--- main_z1.py
+++ main_z1.py
@@ -16,6 +16,25 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        avg_grade = self._calculate_avg_grade()
+        courses_in_progress = ', '.join(self.courses_in_progress)
+        finished_courses = ', '.join(self.finished_courses)
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {avg_grade}\nКурсы в процессе изучения: {courses_in_progress}\nЗавершенные курсы: {finished_courses}"
+
+    def _calculate_avg_grade(self):
+        total = 0
+        count = 0
+        for grades in self.grades.values():
+            total += sum(grades)
+            count += len(grades)
+        return round(total / count, 1) if count > 0 else 0
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+        return self._calculate_avg_grade() < other._calculate_avg_grade()
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -23,11 +42,31 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
+
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def __str__(self):
+        avg_grade = self._calculate_avg_grade()
+        return f"{super().__str__()}\nСредняя оценка за лекции: {avg_grade}"
+
+    def _calculate_avg_grade(self):
+        total = 0
+        count = 0
+        for grades in self.grades.values():
+            total += sum(grades)
+            count += len(grades)
+        return round(total / count, 1) if count > 0 else 0
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+        return self._calculate_avg_grade() < other._calculate_avg_grade()
 
 
 class Reviewer(Mentor):
@@ -42,3 +81,6 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return super().__str__()
